d34af63dda86ae92ebf83a2de35a895ad46b05ae
MAA-98 <marek.a.kurczynski@gmail.com>
2025-07-19 21:05:48 +0100

    No recent apps in Dock

--- debloat_mac.sh
+++ debloat_mac.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
+# Bloatware apps not part of system to be deleted:
 BLOAT_APPS=(
   "/Applications/GarageBand.app"
   "/Applications/Keynote.app"
   "/Applications/Numbers.app"
   "/Applications/iMovie.app"
 )
-
 for app in "${BLOAT_APPS[@]}"; do
   if [ -d "$app" ]; then
     echo "Deleting $app ..."
@@ -23,18 +23,22 @@ done
 
 echo "Deletion process complete."
 
+# Only allow Spotlight to index Applications: I only use it for opening apps
 echo "Disabling Spotlight indexing globally..."
 sudo mdutil -a -i off
-
 echo "Enabling Spotlight indexing only for /Applications..."
 sudo mdutil -i on /Applications
-
 echo "Adding /Users and /System to Spotlight Privacy (exclude indexing)..."
 sudo defaults write /Library/Preferences/com.apple.Spotlight.plist Exclusions -array "/Users" "/System" "/Library" "/private"
-
 echo "Restarting Spotlight indexing daemon..."
 sudo killall mds
-
 echo "Done. Spotlight now indexes only /Applications."
 mdutil -sa
 
+# Get rid of recent apps in the Dock
+echo "Disabling recent apps in Dock..."
+# Set show-recents to false
+defaults write com.apple.dock show-recents -bool false
+# Restart the Dock to apply changes
+killall Dock
+echo "Recent apps in Dock have been disabled."
